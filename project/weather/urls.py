5bec4f135a475818dea03636e161565b87dbf8d5
Ivan <vanechka-nikitin-2004@mail.ru>
2024-07-19 12:22:45 +0800

    Add ez read weather and add API for count city

--- project/weather/urls.py
+++ project/weather/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import get_weather
+from .views import get_weather, search_statistics
+
+app_name = 'weather'
 
 urlpatterns = [
     path('', get_weather, name='get_weather'),
+    path('statistics/', search_statistics, name='search_statistics'),
 ]
--- project/weather/views.py
+++ project/weather/views.py
@@ -1,6 +1,7 @@
 import requests
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.utils import timezone
 
 from .models import WeatherQuery
 
@@ -22,14 +23,28 @@ def get_weather(request):
                     f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m')
                 weather_data = weather_response.json()
 
+                # Фильтрация данных только на сегодняшнюю дату
+                current_date = timezone.now().strftime('%Y-%m-%d')
+                hourly_data = list(zip(weather_data['hourly']['time'], weather_data['hourly']['temperature_2m']))
+                today_data = [(time.split('T')[1], temp) for time, temp in hourly_data if time.startswith(current_date)]
+
                 weather_query, created = WeatherQuery.objects.get_or_create(city=city)
                 if not created:
                     weather_query.query_count += 1
                     weather_query.save()
 
-                return render(request, 'weather/weather.html', {'weather_data': weather_data})
+                return render(request, 'weather/weather.html', {
+                    'weather_data': today_data,
+                    'city': city,
+                    'current_date': current_date,
+                })
             else:
                 error_message = 'Город не найден'
                 return render(request, 'weather/weather.html', {'error_message': error_message})
 
     return render(request, 'weather/weather.html')
+
+
+def search_statistics(request):
+    statistics = WeatherQuery.objects.all().values('city', 'query_count')
+    return JsonResponse(list(statistics), safe=False)
