a5ab9f785c062e99df4ecd00121a4f94eec9a789
Ivan <vanechka.nikitin.20044@gmail.com>
2024-07-19 16:56:24 +0800

    Add comments

--- project/weather/templatetags/custom_tags.py
+++ project/weather/templatetags/custom_tags.py
@@ -5,4 +5,5 @@ register = template.Library()
 
 @register.filter
 def zip_lists(a, b):
+    # Объединяет два списка, создавая список кортежей, где каждый кортеж состоит из элементов обоих списков с одинаковыми индексами.
     return zip(a, b)
--- project/weather/views.py
+++ project/weather/views.py
@@ -1,15 +1,17 @@
-import requests
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.utils import timezone
 
 from .models import WeatherQuery
 
+import requests
+
 
 def get_weather(request):
     show_last_city_modal = False
     last_city = None
 
+    # Проверка, если в сессии есть последний просмотренный город и параметр 'city' не передан
     if 'last_city' in request.session and not request.GET.get('city'):
         show_last_city_modal = True
         last_city = request.session['last_city']
@@ -35,18 +37,20 @@ def get_weather(request):
                 hourly_data = list(zip(weather_data['hourly']['time'], weather_data['hourly']['temperature_2m']))
                 today_data = [(time.split('T')[1], temp) for time, temp in hourly_data if time.startswith(current_date)]
 
+                # Обновление или создание записи в базе данных для данного города
                 weather_query, created = WeatherQuery.objects.get_or_create(city=city)
                 if not created:
                     weather_query.query_count += 1
                     weather_query.save()
 
-                # Сохранение истории поиска
+                # Сохранение истории поиска в сессии
                 if 'search_history' not in request.session:
                     request.session['search_history'] = []
                 if city not in request.session['search_history']:
                     request.session['search_history'].append(city)
                 request.session['last_city'] = city
 
+                # Рендеринг страницы с данными о погоде
                 return render(request, 'weather/weather.html', {
                     'weather_data': today_data,
                     'city': city,
@@ -55,6 +59,7 @@ def get_weather(request):
                     'last_city': last_city,
                 })
             else:
+                # Если город не найден, отображаем сообщение об ошибке
                 error_message = 'City not found.'
                 return render(request, 'weather/weather.html', {
                     'error_message': error_message,
@@ -62,6 +67,7 @@ def get_weather(request):
                     'last_city': last_city,
                 })
 
+    # Если метод запроса не GET, просто рендерим страницу без данных о погоде
     return render(request, 'weather/weather.html', {
         'show_last_city_modal': show_last_city_modal,
         'last_city': last_city,
@@ -69,15 +75,18 @@ def get_weather(request):
 
 
 def search_statistics(request):
+    # Получение статистики запросов по городам
     statistics = WeatherQuery.objects.all().values('city', 'query_count')
     return JsonResponse(list(statistics), safe=False)
 
 
 def get_history(request):
+    # Получение истории поиска из сессии
     history = request.session.get('search_history', [])
     return JsonResponse({'history': history})
 
 
 def get_last_city(request):
+    # Получение последнего просмотренного города из сессии
     last_city = request.session.get('last_city', None)
     return JsonResponse({'last_city': last_city})
