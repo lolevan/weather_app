1dd1cc855c1fa143cb66e6f343bee45f3590ede0
Ivan <vanechka-nikitin-2004@mail.ru>
2024-07-19 13:40:33 +0800

    Add hisotory and notify last city

--- project/weather/urls.py
+++ project/weather/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_weather, search_statistics
+from .views import get_weather, search_statistics, get_history, get_last_city
 
-app_name = 'weather'
 
 urlpatterns = [
     path('', get_weather, name='get_weather'),
     path('statistics/', search_statistics, name='search_statistics'),
+    path('history/', get_history, name='get_history'),
+    path('last_city/', get_last_city, name='get_last_city'),
 ]
--- project/weather/views.py
+++ project/weather/views.py
@@ -1,5 +1,5 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.utils import timezone
 
@@ -7,6 +7,13 @@ from .models import WeatherQuery
 
 
 def get_weather(request):
+    show_last_city_modal = False
+    last_city = None
+
+    if 'last_city' in request.session and not request.GET.get('city'):
+        show_last_city_modal = True
+        last_city = request.session['last_city']
+
     if request.method == 'GET':
         city = request.GET.get('city')
         if city:
@@ -33,18 +40,44 @@ def get_weather(request):
                     weather_query.query_count += 1
                     weather_query.save()
 
+                # Сохранение истории поиска
+                if 'search_history' not in request.session:
+                    request.session['search_history'] = []
+                if city not in request.session['search_history']:
+                    request.session['search_history'].append(city)
+                request.session['last_city'] = city
+
                 return render(request, 'weather/weather.html', {
                     'weather_data': today_data,
                     'city': city,
                     'current_date': current_date,
+                    'show_last_city_modal': show_last_city_modal,
+                    'last_city': last_city,
                 })
             else:
-                error_message = 'Город не найден'
-                return render(request, 'weather/weather.html', {'error_message': error_message})
+                error_message = 'City not found.'
+                return render(request, 'weather/weather.html', {
+                    'error_message': error_message,
+                    'show_last_city_modal': show_last_city_modal,
+                    'last_city': last_city,
+                })
 
-    return render(request, 'weather/weather.html')
+    return render(request, 'weather/weather.html', {
+        'show_last_city_modal': show_last_city_modal,
+        'last_city': last_city,
+    })
 
 
 def search_statistics(request):
     statistics = WeatherQuery.objects.all().values('city', 'query_count')
     return JsonResponse(list(statistics), safe=False)
+
+
+def get_history(request):
+    history = request.session.get('search_history', [])
+    return JsonResponse({'history': history})
+
+
+def get_last_city(request):
+    last_city = request.session.get('last_city', None)
+    return JsonResponse({'last_city': last_city})
